daeba54ea4b23811e786ae8c852ccf6d8e355aaa
kuanchiliao1 <kuanchiliao@gmail.com>
2023-10-09 17:46:27 -0700

    Implement basic card functionality

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,26 @@
-export default function Card() {
-  return (
-    <div className="card">
+import { useEffect, useState } from "react";
+import scrambleArray from "../utils"
 
+export default function Card({index, useCards}) {
+  const [url, setUrl] = useState('');
+
+  function shuffleCards() {
+    useCards(prevCards => scrambleArray(prevCards))
+  }
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${index +1}`)
+    .then(response => response.json())
+    .then(data => {
+      setUrl(data.sprites.front_default);
+      console.log(data.sprites.front_default);
+    });
+  })
+
+  return (
+    <div onClick={shuffleCards} className="card">
+      <h1>Card</h1>
+      <img src={url} alt="" />
     </div>
   )
 }
